4ba1293dc36a23aeb4c310d5a2b9e1c9c0f91bce
Ebihara Yusuke <ebiyuu1121@gmail.com>
2020-11-22 14:41:46 +0900

    1.6

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const pointClickSize = 50;
 let gridPoint = [];
 let draggingItem = null;
 let edge = [];
+let draggingEdge = null;
 
 // 点の位置を計算する
 for (let i = 0; i < sizeX + 1; i++) {
@@ -25,7 +26,6 @@ for (let i = 0; i < sizeX + 1; i++) {
 }
 
 // 点を打つ
-
 function draw() {
   ctx.clearRect(0, 0, width, height);
 
@@ -45,67 +45,77 @@ function draw() {
     const endPoint = gridPoint[e[1].i][e[1].j];
     line(startPoint, endPoint);
   });
+
+  if (draggingEdge) {
+    line(draggingEdge[0], draggingEdge[1]);
+  }
 }
 draw();
 
-// クリックされたときの処理
+// マウスを押したときの処理
 canvas.addEventListener("mousedown", click);
 function click(event) {
   // 座標取得
-  const rect = canvas.getBoundingClientRect();
-  const x = ((event.clientX - rect.left) / canvas.clientWidth) * width;
-  const y = ((event.clientY - rect.top) / canvas.clientHeight) * height;
-
-  gridPoint.forEach((row, i) =>
-    row.forEach((point, j) => {
-      if ((point.x - x) ** 2 + (point.y - y) ** 2 < pointClickSize ** 2) {
-        draggingItem = { i: i, j: j };
-      }
-    })
-  );
+  const { x, y } = getXY(event, canvas);
+  const ij = xy2ij({ x, y }, pointClickSize);
+  if (ij) {
+    draggingItem = { ...ij };
+  }
 }
 
+// マウスを動かしたときの処理
 canvas.addEventListener("mousemove", move);
 function move(event) {
   if (!draggingItem) return;
 
-  // 座標取得
-  const rect = canvas.getBoundingClientRect();
-  const x = ((event.clientX - rect.left) / canvas.clientWidth) * width;
-  const y = ((event.clientY - rect.top) / canvas.clientHeight) * height;
+  // もし点の上にいたら
+  const { x, y } = getXY(event, canvas);
+  ij = xy2ij({ x, y }, pointClickSize / 3);
+  if (ij) {
+    const { i, j } = ij;
+    if (draggingItem.i !== i || draggingItem.j !== j) {
+      if (addEdge(draggingItem, ij)) {
+        draggingItem = ij;
+      }
+    }
+  }
 
-  draw();
   const startPoint = gridPoint[draggingItem.i][draggingItem.j];
-
-  line(startPoint, { x, y });
+  draggingEdge = [startPoint, { x, y }];
+  draw();
 }
 
+// マウスを離したときの処理
 canvas.addEventListener("mouseup", up);
 function up(event) {
   if (!draggingItem) return;
+  draggingEdge = null;
   // 座標取得
-  const rect = canvas.getBoundingClientRect();
-  const x = ((event.clientX - rect.left) / canvas.clientWidth) * width;
-  const y = ((event.clientY - rect.top) / canvas.clientHeight) * height;
+  const { x, y } = getXY(event, canvas);
 
-  gridPoint.forEach((row, i) =>
-    row.forEach((point, j) => {
-      if ((point.x - x) ** 2 + (point.y - y) ** 2 < pointClickSize ** 2) {
-        // ちゃんと隣かどうかチェックする
-        if (draggingItem.i === i) {
-          if (draggingItem.j - j === 1 || draggingItem.j - j === -1) {
-            edge.push([draggingItem, { i, j }]);
-          }
-        } else if (draggingItem.j === j) {
-          if (draggingItem.i - i === 1 || draggingItem.i - i === -1) {
-            edge.push([draggingItem, { i, j }]);
-          }
-        }
-      }
-    })
-  );
+  const ij = xy2ij({ x, y }, pointClickSize);
+  if (ij) {
+    addEdge(draggingItem, ij);
+  }
+
   draw();
   draggingItem = null;
+}
+function addEdge(from, to) {
+  const { i, j } = to;
+  // ちゃんと隣かどうかチェックする
+  if (from.i === i) {
+    if (from.j - j === 1 || from.j - j === -1) {
+      edge.push([from, { i, j }]);
+      return true;
+    }
+  } else if (from.j === j) {
+    if (from.i - i === 1 || from.i - i === -1) {
+      edge.push([from, { i, j }]);
+      return true;
+    }
+  }
+  return false;
 }
 
 function line(from, to) {
@@ -115,4 +125,23 @@ function line(from, to) {
   ctx.moveTo(from.x, from.y);
   ctx.lineTo(to.x, to.y);
   ctx.stroke();
+}
+
+function getXY(event, canvas) {
+  const rect = canvas.getBoundingClientRect();
+  const x = ((event.clientX - rect.left) / canvas.clientWidth) * width;
+  const y = ((event.clientY - rect.top) / canvas.clientHeight) * height;
+  return { x, y };
+}
+
+function xy2ij(point, size) {
+  let ij = null;
+  gridPoint.forEach((row, i) =>
+    row.forEach(({ x, y }, j) => {
+      if ((point.x - x) ** 2 + (point.y - y) ** 2 < size ** 2) {
+        ij = { i, j };
+      }
+    })
+  );
+  return ij;
 }
