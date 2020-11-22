4ee363f1c1f62461d9c7b163d5335a6a1ae21609
Ebihara Yusuke <ebiyuu1121@gmail.com>
2020-11-22 12:12:01 +0900

    a

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const width = 500;
 const sizeX = 5;
 const sizeY = 5;
 
+const pointSize = 10;
+
 for (let i = 0; i < sizeX + 1; i++) {
   const x = ((i + 0.5) / (sizeX + 1)) * width;
   for (let j = 0; j < sizeY + 1; j++) {
@@ -15,4 +17,14 @@ for (let i = 0; i < sizeX + 1; i++) {
     ctx.arc(x, y, 10, 0, 2 * Math.PI);
     ctx.fill();
   }
+}
+
+canvas.addEventListener("click", click);
+
+function click(event) {
+  // 座標取得
+  const rect = canvas.getBoundingClientRect();
+  const x = (event.clientX - rect.left) / canvas.clientWidth;
+  const y = (event.clientY - rect.top) / canvas.clientHeight;
+  console.log({ x, y });
 }
